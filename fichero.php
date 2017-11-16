e12953786b886c47d02d35ad423d01241c84066a
jaramos1990 <jantoniort1990@gmail.com>
2017-11-16 16:46:20 +0100

    #modificación del fichero

--- fichero.php
+++ fichero.php
@@ -1,4 +1,9 @@
-<?php
-echo "holaaa";
+<html>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+	<a href="www.sevillafc.es">Pinchar aquí</a>
+ <?php echo '<p>Hola Mundo</p>'; ?>
+ </body>
+</html>
 
-?>
